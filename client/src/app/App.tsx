7e8a513353abc673f7bef49538587af3901a1d17
Alexander <alexandrsuchkov67@gmail.com>
2021-10-07 23:18:30 +0300

    added main files for project

---
+++ client/src/app/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import ActionTile from "components/ActionTile";
+
+const App = () => {
+  return (
+    <div className="App">
+      <ActionTile/>
+    </div>
+  );
+};
+
+export default App;
---
+++ client/src/index.tsx
@@ -0,0 +1,15 @@
+import * as React from "react";
+import { render } from "react-dom";
+
+import App from "./app/App";
+
+import "styles/index.scss";
+
+render(
+  <App />, 
+  document.getElementById("root")
+);
+
+if (module.hot) {
+  module.hot.accept();
+}
