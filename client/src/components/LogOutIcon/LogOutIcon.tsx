8c72f8b6ebd036c7b21ef15db8ac43631595aa13
Alexander <alexandrsuchkov67@gmail.com>
2021-10-08 13:36:25 +0300

    added logout icon for button

---
+++ client/src/components/LogOutIcon/LogOutIcon.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const LogOutIcon: React.FC = () => {
+  return (
+    <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <g clipPath="url(#clip0)">
+        <path d="M6 5.25V4.1875C6 3.34212 6.31607 2.53137 6.87868 1.9336C7.44129 1.33582 8.20435 1 9 1H16C16.7956 1 17.5587 1.33582 18.1213 1.9336C18.6839 2.53137 19 3.34212 19 4.1875V14.8125C19 15.6579 18.6839 16.4686 18.1213 17.0664C17.5587 17.6642 16.7956 18 16 18H9C8.20435 18 7.44129 17.6642 6.87868 17.0664C6.31607 16.4686 6 15.6579 6 14.8125V13.75M11 5.25L15 9.5L11 5.25ZM15 9.5L11 13.75L15 9.5ZM15 9.5L1 9.5L15 9.5Z" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+      </g>
+
+      <defs>
+        <clipPath id="clip0">
+          <rect width="20" height="20" fill="white" transform="translate(20 20) rotate(-180)"/>
+        </clipPath>
+      </defs>
+    </svg>
+  );
+};
+
+export default LogOutIcon;
