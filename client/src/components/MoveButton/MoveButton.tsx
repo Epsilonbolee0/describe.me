cca059aa82a6d3ea9e6aae64daf2bdc2f8bb6aa4
Alexander <alexandrsuchkov67@gmail.com>
2021-12-17 00:04:34 +0300

    updated move button

--- client/src/components/MoveButton/MoveButton.tsx
+++ client/src/components/MoveButton/MoveButton.tsx
@@ -3,17 +3,18 @@ import React from 'react';
 import styles from "./MoveButton.module.scss";
 
 type MoveButtonProps = {
-  children1: string | React.ReactNode,
+  children1?: string | React.ReactNode,
   children2: string | React.ReactNode
 }
 
-const MoveButton: React.FC<MoveButtonProps> = ({children1, children2}) => {
+const MoveButton: React.FC<MoveButtonProps> = ({ children1, children2 }) => {
+
   return (
     <button className={styles.move_button}>
-      {typeof children1 === "string" ? 
-        <p className={styles.btn_text}>{children1}</p> : 
-        <div className={styles.btn_icon}>{children1}</div>
-      }
+      {children1 && (typeof children1 === "string" ? 
+        <p className={`${styles.btn_text} ${styles.left_item}`}>{children1}</p> : 
+        <div className={`${styles.btn_icon} ${styles.left_item}`}>{children1}</div>
+      )}
 
       {typeof children2 === "string" ? 
         <p className={styles.btn_text}>{children2}</p> : 
